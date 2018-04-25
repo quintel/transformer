482a6af6b3beb87748ba419bd41a174f7ca529ef
Gerard Westerhof <gerard@grdw.nl>
2018-04-25 15:41:11 +0200

    Add methods to fetch attributes from nodes and edges

--- lib/transformer/runtime.rb
+++ lib/transformer/runtime.rb
@@ -15,5 +15,17 @@ module Transformer
     def SPARSE_GRAPH_QUERY(part, method)
       execute(::Atlas::SparseGraphQuery.find(:"#{part}+#{method}").query)
     end
+
+    def NODE_VALUE(node_key, attribute)
+      Atlas::Node.find(node_key).public_send(attribute)
+    end
+
+    def NODE_EFFICIENCY(node_key, direction, carrier)
+      Atlas::Node.find(node_key).public_send(direction).fetch(carrier)
+    end
+
+    def EDGE_VALUE(edge_key, attribute)
+      Atlas::Edge.find(edge_key).public_send(attribute)
+    end
   end
 end
